40f75586198c2f935e9726e6f2a3513783cbcf29
Mirko Geria <mirkogeria@gmail.com>
2026-05-14 15:37:45 +0200

    chore: remove old terraform and k8s structure to refactor

--- terraform/environments/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "orderflow-terraform-state"
-    key            = "staging/terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-    dynamodb_table = "orderflow-terraform-locks"
-  }
-}
